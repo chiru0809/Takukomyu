40b90e1e50f204bace186fac8cb17d97fecb8e16
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-09-06 12:09:50 +0000

    [Fix] migration

--- db/schema.rb
+++ db/schema.rb
@@ -111,12 +111,6 @@ ActiveRecord::Schema.define(version: 2023_09_04_050353) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.integer "title_id"
-    t.integer "main_playstyle"
-    t.text "message"
-    t.boolean "is_private", default: false
-    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
